32fd380a93ec6c8f73cb5de7080c5c4faef576b3
Hans Kirchner <hans-kirchner@gmx.de>
2015-11-30 00:45:29 +0100

    device-ip: Save current date, Fix XSS

--- device-ip-backend.php
+++ device-ip-backend.php
@@ -1,13 +1,16 @@
 <?php
 define("MAX_DATA_SIZE", 100);
 
-if(isset($_POST['data']) && strlen($_POST['data']) <= MAX_DATA_SIZE) {
-    if(file_put_contents('data.txt', $_POST['data'])) {
+$data = $_POST['data'];
+
+if(isset($data) && strlen($data) <= MAX_DATA_SIZE) {
+    $data = date('Y-m-d H:i:s') . ' ' . $data;
+    if(file_put_contents('data.txt', $data)) {
         echo "success";
     } else {
         echo "fail";
     }
 } else {
-    echo file_get_contents('data.txt');
+    echo htmlspecialchars(file_get_contents('data.txt'));
 }
 ?>
